6d6ac0494776899dd4eefea67aace7288247a0df
susan <marily.susan@gmail.com>
2026-08-26 21:28:02 -0400

    feat: agregar funcionalidad al boton restar

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ let contador = 0;
 // Buscamos los elementos del HTML que vamos a usar
 const textoContador = document.getElementById("contador");
 const botonSumar = document.getElementById("btn-sumar");
+const botonRestar = document.getElementById("btn-restar");
 
 // Cada clic en el boton suma 1 y actualiza lo que se ve en pantalla
 botonSumar.addEventListener("click", function () {
   contador = contador + 1;
+  textoContador.textContent = contador;
+});
+
+// Cada clic en el boton reduce 1 y actualiza lo que se ve en pantalla
+botonRestar.addEventListener("click", function () {
+  contador = contador - 1;
   textoContador.textContent = contador;
 });
